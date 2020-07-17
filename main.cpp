3f290d50887d0bda9aa46c0a4257ad5143522aee
ataylor <alan.taylor@kenmei.ai>
2020-07-17 23:29:24 +0200

    formatting

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <string>
 #include <fstream>
 
-using namespace std; 
+using namespace std;
 
 #define CHECK_ERR(err, msg)                        \
   {                                                \
@@ -18,27 +18,27 @@ using namespace std;
 
 
 void test_inflate(Byte *compr, uLong comprLen, Byte *uncompr, uLong *uncomprLen) {
-	
-  int err;
-  z_stream d_stream; /* decompression stream */
-  d_stream.zalloc = NULL;
-  d_stream.zfree = NULL;
-  d_stream.opaque = NULL;
-  d_stream.next_in = compr;
-  d_stream.avail_in = 0;
-  d_stream.next_out = uncompr;
-  err = inflateInit2(&d_stream, MAX_WBITS + 16);
-  CHECK_ERR(err, "inflateInit");
 
-  while (d_stream.total_out < *uncomprLen && d_stream.total_in < comprLen) {
-    d_stream.avail_in = d_stream.avail_out = 1; /* force small buffers */
-    err = inflate(&d_stream, Z_NO_FLUSH);
-    if (err == Z_STREAM_END)
-      break;
-    CHECK_ERR(err, "inflate");
-  }
-  err = inflateEnd(&d_stream);
-  *uncomprLen = d_stream.total_out;
+	int err;
+	z_stream d_stream; /* decompression stream */
+	d_stream.zalloc = NULL;
+	d_stream.zfree = NULL;
+	d_stream.opaque = NULL;
+	d_stream.next_in = compr;
+	d_stream.avail_in = 0;
+	d_stream.next_out = uncompr;
+	err = inflateInit2(&d_stream, MAX_WBITS + 16);
+	CHECK_ERR(err, "inflateInit");
+
+	while (d_stream.total_out < *uncomprLen && d_stream.total_in < comprLen) {
+		d_stream.avail_in = d_stream.avail_out = 1; /* force small buffers */
+		err = inflate(&d_stream, Z_NO_FLUSH);
+		if (err == Z_STREAM_END)
+			break;
+		CHECK_ERR(err, "inflate");
+	}
+	err = inflateEnd(&d_stream);
+	*uncomprLen = d_stream.total_out;
 
 }
 
@@ -84,12 +84,12 @@ int main() {
 	ifs.read(data, len);
 	ifs.close();
 
-    uLong buffer_out_size = (len * 10); // initial value 10 times bigger than input size, later will be overwritten with the correct output size
-    char *buffer_out = new char[buffer_out_size];
+	uLong buffer_out_size = (len * 10); // initial value 10 times bigger than input size, later will be overwritten with the correct output size
+	char *buffer_out = new char[buffer_out_size];
 
-    test_inflate((Byte *)data, len, (Byte *)buffer_out, &buffer_out_size);
+	test_inflate((Byte *)data, len, (Byte *)buffer_out, &buffer_out_size);
 
-    std::cout << "size of unzipped data via char array: " << buffer_out_size << std::endl; 
+	std::cout << "size of unzipped data via char array: " << buffer_out_size << std::endl;
 
 	return 0;
 }
